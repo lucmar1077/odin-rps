b011a6a08f67ff243c890be38c37f6513d1dfe37
Lucas <lucmar1077@gmail.com>
2024-09-16 09:06:30 -0600

    Add logic for computer choice

---
+++ script.js
@@ -0,0 +1,21 @@
+let computerChoice = 0;
+function getComputerChoice() {
+
+    computerChoice = Math.floor(Math.random() * 3);
+
+    switch (computerChoice) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2: 
+            computerChoice = "Scissors"
+            break;   
+    }
+}
+
+// Prompt the user for their choice
+// Go through the logic --> 
+// if 
